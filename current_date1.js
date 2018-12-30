21487d0969b2b9c163429b3c9faf93878f919309
abhinay-bandooni <46245321+abhinay-bandooni@users.noreply.github.com>
2018-12-30 19:55:42 +0530

    Update current_date1.js

--- current_date1.js
+++ current_date1.js
@@ -1,6 +1,3 @@
-//Today is: Tuesday
-//Time: 10:38:30pm
-
 let date = new Date();
 let days_of_week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
@@ -10,5 +7,4 @@ let min = (date.getMinutes()<10? "0" + date.getMinutes() : date.getMinutes());
 let hour = (date.getHours()<12? date.getHours() : date.getHours()%12);
 hour = (hour<10? '0' + hour: hour);
 console.log('Today is: '+days_of_week[date.getDay()]);
-// console.log('Time is: ' + date.getHours()%12 + ':' + date.getMinutes() + ':' + date.getSeconds() + (date.getHours<=12)?'AM':'PM');
 console.log('Time is: ' + hour + ':' +min + ':' + sec + ampm);
